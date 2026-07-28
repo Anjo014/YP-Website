d7fdf0feea74e49c60625287b0ed2e6b540792f4
Anjo014 <92507241+Anjo014@users.noreply.github.com>
2026-07-28 13:19:33 +0800

    Delete js directory

--- js/config.js
+++
@@ -1,3 +0,0 @@
-// Paste your deployed Apps Script Web App URL here (ends in /exec).
-// See README.md for how to get this.
-const API_URL = 'https://script.google.com/macros/s/AKfycbwwjf87uRaBNrTbvlhaQhpuL_nDaMLpAZCVa-R0jEFEvjClFQWiKgSlWv6SSF2c8qoLHQ/exec';
