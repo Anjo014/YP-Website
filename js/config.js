9fc60aeaa349405181f85fc296ba2832be194338
Anjo014 <92507241+Anjo014@users.noreply.github.com>
2026-07-28 19:03:47 +0800

    Update API_URL to new deployed Web App URL

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Paste your deployed Apps Script Web App URL here (ends in /exec).
 // See README.md for how to get this.
-const API_URL = 'https://script.google.com/macros/s/AKfycbyVG5GsHgQMGRvZwqYL8zhL-ojIKF1k4rTPXgL1fonTK-plbQCmpHxnkNlzYeJSxtrxww/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzHAos2OkZp-g4mwvqgry19xmUy6UfAM8zAxrCEPhgNXFfSFYOyA29cCtGHHqp_1bY2KA/exec';
