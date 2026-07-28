66b7c1b5e5972babaacdc197dc82cf4d7baf855c
Anjo014 <92507241+Anjo014@users.noreply.github.com>
2026-07-28 13:20:48 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// Paste your deployed Apps Script Web App URL here (ends in /exec).
+// See README.md for how to get this.
+const API_URL = 'https://script.google.com/macros/s/AKfycbxJ93mR-4K4k_MqHG4Ow_Bk7--1eJVsO7s0_59jKqoMgf_hBqDjrvHRMVXi-5UwCEGx_Q/exec';
